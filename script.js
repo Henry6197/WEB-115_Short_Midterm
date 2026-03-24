153c93e9fe24d8b54038d61b0f174bdbb34c32f3
Henry Ervin Short <heshort@my.waketech.edu>
2026-03-24 14:12:07 -0400

    done :sob:

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 function calculateAmortization() {
     const principal = parseFloat(document.getElementById('loanamount').value);
-    const annualRate = parseFloat(document.getElementById('intrestrate').value)/100;
+    const annualRate = 5.75/100;
     const years = parseFloat(document.getElementById('loanterm').value);
-    const monthlyrate = annualRate/12;
-    const numberofpayments=years*12
-    const x = Math.pow(1+monthlyrate, numberofpayments);
-    const monthlypayments = (principal * x * monthlyrate)/(x-1);
-    let remainingbalance=principal;
-    const tablebody = document.getElementById('schedulebody');
-    tablebody.innerHTML = "";
-        for(let i=1; i<= numberofpayments; i++){
-            const intrestpayment = remainingbalance * monthlyrate;
-            const principalpaid = monthlypayments - intrestpayment;
-            remainingbalance -= principalpaid;
-            const row = HTMLBaseElement<tr>
-            HTMLBaseElement<td>$<script>{i} 
-            HTMLBaseElement<td>$<script> monthlypayments.toFixed(2)
-            HTMLBaseElement<td>$principalpaid.toFixed(2)
-            HTMLBaseElement<td>$intrestpayment.toFixed(2)
-            HTMLBaseElement<td>$Math.max(0, remainingbalance).toFixed(2);
-        }
-        tablebody.innerHTML+=row;
-}
+    if (years === 15) or (years === 30);
+        const monthlyrate = annualRate/12;
+        const numberofpayments=years*12
+        const x = Math.pow(1+monthlyrate, numberofpayments);
+        const monthlypayments = (principal * x * monthlyrate)/(x-1);
+        let remainingbalance=principal;
+        const tablebody = document.getElementById('schedulebody');
+        tablebody.innerHTML = "";
+            for(let i=1; i<= numberofpayments; i++){
+                const intrestpayment = remainingbalance * monthlyrate;
+                const principalpaid = monthlypayments - intrestpayment;
+                remainingbalance -= principalpaid;
+                const row = HTMLDivElement<tr>
+                HTMLBaseElement<td>$<script>{i} 
+                HTMLBaseElement<td>$<script> monthlypayments.toFixed(2)
+                HTMLBaseElement<td>$principalpaid.toFixed(2)
+                HTMLBaseElement<td>$intrestpayment.toFixed(2)
+                HTMLBaseElement<td>$Math.max(0, remainingbalance).toFixed(2);
+            }
+            tablebody.innerHTML+=row;
+    }
+    else; 
